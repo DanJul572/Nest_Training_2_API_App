4c25988d9c41d1abe2fc9d9436e30e2b5a184900
Dandi <dandi.juliandi@nawadata.id>
2024-08-11 14:03:21 +0700

    add validation when password is invalid

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,9 @@
 import * as bcrypt from 'bcrypt';
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UsersService } from 'src/users/users.service';
 import { SignInDto } from './dto/signin-dto';
@@ -11,18 +15,39 @@ export class AuthService {
     private jwtService: JwtService,
   ) {}
 
-  public async signIn(signInDto: SignInDto): Promise<any> {
-    const user = await this.usersService.findOneByName(signInDto.username);
-    const isMatchPassword = await bcrypt.compare(
-      signInDto.password,
-      user.password,
-    );
-    if (!isMatchPassword) {
-      throw new UnauthorizedException();
-    }
-    const payload = { sub: user.id, username: user.name };
+  private async isValidPassword(
+    requestPassword: string,
+    existingPassword: string,
+  ): Promise<boolean> {
+    return await bcrypt.compare(requestPassword, existingPassword);
+  }
+
+  private async generateJWTToken(
+    id: number,
+    name: string,
+  ): Promise<{
+    access_token: string;
+  }> {
+    const payload = { sub: id, username: name };
     return {
       access_token: await this.jwtService.signAsync(payload),
     };
+  }
+
+  public async signIn(signInDto: SignInDto): Promise<any> {
+    const user = await this.usersService.findOneByName(signInDto.username);
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+
+    const isValidPassword = await this.isValidPassword(
+      signInDto.password,
+      user.password,
+    );
+    if (!isValidPassword) {
+      throw new BadRequestException('password is invalid');
+    }
+
+    return this.generateJWTToken(user.id, user.name);
   }
 }
